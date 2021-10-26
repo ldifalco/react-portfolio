00410bffc7082335df434c7bf4472b5ee3b6f635
Lucas <luke.difalco52@gmail.com>
2021-10-25 23:02:39 -0400

    skeleton about me section started, will stylize once all pages are working

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="about-me-container">
+            <p className="bio">A Navy veteran passionate about technology, gaming, and sociolgy. While having experience in occupations from retail, to healthcare, to serving, stepping into the world of tech for the first time is bringing excitement for new possibilites. Aiming to develop a strong technical knowledge base and relationships to increase overall morale and client satisfaction. Driven best by building mutually beneficial professional relationships.</p>
+        </div>
+    )
+}
+export default About
